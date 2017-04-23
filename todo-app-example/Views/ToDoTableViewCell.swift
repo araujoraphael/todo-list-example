48dee2353c3a1f44a4333f06f4dac43aed0c3b04
Raphael Araújo <aaraujo.raphael@gmail.com>
2017-04-22 21:43:10 -0300

    show TODO priority

--- todo-app-example/Views/ToDoTableViewCell.swift
+++ todo-app-example/Views/ToDoTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ToDoTableViewCell: UITableViewCell {
     @IBOutlet weak var notePreviewLabel : UILabel?
+    @IBOutlet weak var priorityLabel : UILabel?
     @IBOutlet weak var dateLabel : UILabel?
     @IBOutlet weak var checkImage : UIImage?
     @IBOutlet weak var checkImageLeftOffset : NSLayoutConstraint?
@@ -27,14 +28,21 @@ class ToDoTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.notePreviewLabel?.text = toDo?.name
         self.dateLabel?.text = toDo?.lastUpdatedStr()
+        if(self.toDo?.priority == 0) {
+            self.priorityLabel?.text = "low"
+            self.priorityLabel?.backgroundColor = UIColor(red: 49.0/255, green: 115.0/255, blue: 51.0/255, alpha: 1.0)            }
+        else if (self.toDo?.priority == 1) {
+            self.priorityLabel?.text = "medium"
+            self.priorityLabel?.backgroundColor = UIColor(red: 237.0/255, green: 121.0/255, blue: 6.0/255, alpha: 1.0)
+        } else if (self.toDo?.priority == 2) {
+            self.priorityLabel?.text = "high"
+            self.priorityLabel?.backgroundColor = UIColor(red: 145.0/255, green: 52.0/255, blue: 19.0/255, alpha: 1.0)
+        }
     }
-    
 }
